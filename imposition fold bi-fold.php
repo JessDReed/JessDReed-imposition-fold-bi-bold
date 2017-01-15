de919fa011710533a27cbdacca7d6ebb187b2edd
JessDReed <jessdreed@gmail.com>
2017-01-14 21:17:17 -0700

    Add files via upload

---
+++ imposition fold bi-fold.php
@@ -0,0 +1,58 @@
+?php>
+"IMP001" => array(
+   "id" => "IMP001",
+   "label" => "Fold Bi-fold",
+   "surfaceCount" => 4,
+   "printLayouts" => array(
+    array(
+     "front" => true,
+     "gridX" => true,
+     "gridY" => 0,
+     "rotation" => 0,
+     "bleedLeft" => false,
+     "bleedRight" => true,
+     "bleedTop" => false,
+     "bleedBottom" => true,
+     "mirror" => false,
+     "label" => "Back"
+    ),
+    array(
+     "front" => true,
+     "gridX" => 1,
+     "gridY" => 0,
+     "rotation" => 0,
+     "bleedLeft" => true,
+     "bleedRight" => false,
+     "bleedTop" => false,
+     "bleedBottom" => true,
+     "mirror" => false,
+     "label" => "Front"
+    ),
+    array(
+     "front" => true,
+     "gridX" => 0,
+     "gridY" => 0,
+     "rotation" => 0,
+     "bleedLeft" => true,
+     "bleedRight" => false,
+     "bleedTop" => true,
+     "bleedBottom" => false,
+     "mirror" => false,
+     "label" => "Inside Left"
+    ),
+    array(
+     "front" => true,
+     "gridX" => 1,
+     "gridY" => 0,
+     "rotation" => 0,
+     "bleedLeft" => false,
+     "bleedRight" => true,
+     "bleedTop" => true,
+     "bleedBottom" => false,
+     "mirror" => false,
+     "label" => "Inside Right"
+    )
+   ),
+   "gridHeight" => 1,
+   "gridWidth" => 2
+?>
